072af50c96a5cf6c030d965e6ee31132bd3106a2
mivaldri <marcellivaldrii@gmail.com>
2021-03-17 21:02:34 +0700

    insertionsort in descending(success)

--- Week 1/PolymorphicSorting/src/Sorting.java
+++ Week 1/PolymorphicSorting/src/Sorting.java
@@ -27,11 +27,11 @@ public class Sorting {
 	 // sort algorithm. 
 	 //----------------------------------------------------------------- 
  public static void insertionSort (Comparable[] list) {
-	 for (int index = list.length; index > list.length-1; index--) {
+	 for (int index = 1; index < list.length; index++) {
 		 Comparable key = list[index]; 
 		 int position = index; 
 	 // Shift larger values to the right 
-		 while (position > 0 && key.compareTo(list[position-1]) < 0) {
+		 while (position > 0 && key.compareTo(list[position-1]) > 0) {
 			 list[position] = list[position-1]; 
 			 position--; 
 			 } 
