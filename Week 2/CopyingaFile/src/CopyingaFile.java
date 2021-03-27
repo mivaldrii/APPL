df40e524e093f811e976bcfa2e2f35843106b39f
mivaldri <marcellivaldrii@gmail.com>
2021-03-27 22:05:53 +0700

    add loop

--- Week 2/CopyingaFile/src/CopyingaFile.java
+++ Week 2/CopyingaFile/src/CopyingaFile.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 
 public class CopyingaFile {
 	public static void main(String args[]) {
-		String fileName;
-
-		System.out.println("Enter file name:");
 		
-		Scanner scan = new Scanner(System.in);
-		fileName = scan.nextLine();
-		
-		File text = new File("D:/Akademik/SEM 4/APPL/APPL PR/Week 2/CopyingaFile/src/" + fileName + ".txt");
-		try {
-			scan = new Scanner(text);
-			while (scan.hasNextLine()) {
-				System.out.println(scan.nextLine());
+		while(true){
+			String fileName;
+			
+			System.out.println("Enter file name:");
+			
+			Scanner scan = new Scanner(System.in);
+			fileName = scan.nextLine();
+			
+			File text = new File("D:/Akademik/SEM 4/APPL/APPL PR/Week 2/CopyingaFile/src/" + fileName + ".txt");
+			try {
+				scan = new Scanner(text);
+				while (scan.hasNextLine()) {
+					System.out.println(scan.nextLine());
+				}
+			}
+			catch (FileNotFoundException e) {
+				System.out.println("File Not Found, try again...");
 			}
 		}
-		catch (FileNotFoundException e) {
-			System.out.println("File Not Found, try again...");
-		}
+		
 	}
 }
 
